b908a59927f4eaa601258cc02f176b1796b085b5
JaeeunJKim <kkjjee41@gmail.com>
2024-03-10 22:40:01 +0900

    Add: 옹알이2.py

--- Programmers/옹알이2.py
+++ Programmers/옹알이2.py
@@ -1,6 +1,13 @@
 def solution(babbling):
+    result = 0
+    words = ['aya', 'ye', 'woo', 'ma']
 
-    for word in babbling:
+    for bab in babbling:
+        for word in words:
+            if word * 2 not in bab:
+                bab = bab.replace(word, ' ')
 
+        if bab.isspace():
+            result += 1
 
-    return answer
+    return result
